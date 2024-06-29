38b70f8eb0659235a17a13aeece610dde89475ca
ngokien-3475 <nd.kien1618@gmail.com>
2024-06-29 13:59:29 +0700

    29/06

--- commerce_provide_01/lib/screeens/Home/home_screen.dart
+++ commerce_provide_01/lib/screeens/Home/home_screen.dart
@@ -1,5 +1,5 @@
-import 'package:commerce_provide_01/constants.dart';
 import 'package:flutter/material.dart';
+import 'Widget/home_app_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
@@ -19,34 +19,15 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(
+                SizedBox(
                   height: 30,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      style: IconButton.styleFrom(
-                        backgroundColor: kcontentColor,
-                        padding: const EdgeInsets.all(20),
-                      ),
-                      onPressed: () {},
-                      icon: Image.asset(
-                        "images/icon.png",
-                        height: 20,
-                      ),
-                    ),
-                    IconButton(
-                      style: IconButton.styleFrom(
-                        backgroundColor: kcontentColor,
-                        padding: const EdgeInsets.all(20),
-                      ),
-                      onPressed: () {},
-                      iconSize: 30,
-                      icon: const Icon(Icons.notifications_outlined),
-                    ),
-                  ],
+                // for custom AppBar
+                CustomAppBar(),
+                SizedBox(
+                  height: 20,
                 ),
+                // for search bar
               ],
             )),
       ),
